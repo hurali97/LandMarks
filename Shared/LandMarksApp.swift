1f71acf48d930730083f1f8cf74804910046f318
Hur Ali <hurali97@gmail.com>
2022-01-17 23:29:11 +0500

    Completed the Landmark milestone 1

---
+++ Shared/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  Shared
+//
+//  Created by Hur Ali on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
