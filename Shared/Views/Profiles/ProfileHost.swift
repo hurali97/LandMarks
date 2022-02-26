ee7e8008662b83f5b51edb9ef1034f009984b718
Hur Ali <hurali97@gmail.com>
2022-02-27 01:27:35 +0500

    Added Profile details

---
+++ Shared/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  LandMarks
+//
+//  Created by Hur Ali on 2/27/22.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
